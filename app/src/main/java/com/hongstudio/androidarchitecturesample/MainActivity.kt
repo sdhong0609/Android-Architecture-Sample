a0e6f57ddd4aa60c39d85e7ee5c747901fb68484
Seongdeok Hong <sdhong0609@gmail.com>
2024-11-28 22:22:07 +0900

    feat: 안드로이드에서의 변형된 MVC 패턴 적용

--- app/src/main/java/com/hongstudio/androidarchitecturesample/MainActivity.kt
+++ app/src/main/java/com/hongstudio/androidarchitecturesample/MainActivity.kt
@@ -9,24 +9,23 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var product: Product
+    private lateinit var controller: ProductController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 버튼 클릭 이벤트 처리
+        // Controller 초기화
+        controller = ProductController(this)
+
+        // 버튼 클릭 시 할인 계산 요청
         findViewById<Button>(R.id.calculateDiscountButton).setOnClickListener {
             val productName = findViewById<EditText>(R.id.productNameInput).text.toString()
             val productPrice =
                 findViewById<EditText>(R.id.productPriceInput).text.toString().toDoubleOrNull()
 
             if (productPrice != null) {
-                product = Product(productName, productPrice)
-
-                // Model의 로직 호출 및 UI 업데이트
-                val discountedPrice = product.calculateDiscountedPrice()
-                showDiscountedPrice(product.name, discountedPrice)
+                controller.calculateDiscount(productName, productPrice)
             } else {
                 showError("유효한 가격을 입력하세요.")
             }
@@ -34,13 +33,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     // 할인된 가격을 화면에 표시
-    private fun showDiscountedPrice(name: String, discountedPrice: Double) {
+    fun showDiscountedPrice(name: String, discountedPrice: Double) {
         findViewById<TextView>(R.id.resultTextView).text =
             "상품: $name, 할인된 가격: ${"%.2f".format(discountedPrice)}원"
     }
 
     // 에러 메시지 표시
-    private fun showError(errorMessage: String) {
+    fun showError(errorMessage: String) {
         Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
     }
 }
